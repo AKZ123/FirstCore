ceb4d579e788f75e52ea60e4fb50a4e877117be8
kader <akader080@gmail.com>
2020-04-07 10:39:22 +0600

    Change Password p221

--- FirstCore.Web/Controllers/AccountController.cs
+++ FirstCore.Web/Controllers/AccountController.cs
@@ -364,5 +364,44 @@ namespace FirstCore.Web.Controllers
             }
             return View(model);
         }
+
+        //Part: 121.2
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.GetUserAsync(User);
+
+                if (user == null)
+                {
+                    return RedirectToAction("Login");
+                }
+
+                var result = await userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+
+                await signInManager.RefreshSignInAsync(user);
+                return View("ChangePasswordConfirmation");
+            }
+            return View(model);
+        }
+
+
     }
 }
